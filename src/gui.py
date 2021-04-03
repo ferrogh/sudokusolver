5712c3047b13c32ad1fe3cb6ed4c2f2a01a39b99
ferrogh <ferrohardian@gmail.com>
2021-04-03 22:56:42 +0700

    implement reading gui_output

--- src/gui.py
+++ src/gui.py
@@ -1,9 +1,12 @@
+import os
+import time
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDRectangleFlatButton
-from kivy.uix.popup import Popup
+from kivymd.uix.floatlayout import MDFloatLayout
 
 
 class SelectButton(Button):
@@ -18,7 +21,7 @@ class SelectPopup(Popup):
         self.dismiss()
 
 
-class SudokuButton(MDRectangleFlatButton):
+class SudokuCell(MDRectangleFlatButton):
     value = StringProperty(None)
 
     def __init__(self, **kwargs):
@@ -33,13 +36,77 @@ class SudokuButton(MDRectangleFlatButton):
         self.value = value
 
 
-class Sudoku(MDBoxLayout):
-    def print(self):
-        with open("io/gui_input.txt", "w") as f:
+class SudokuBox(MDBoxLayout):
+    pass
+
+
+class SolvePopup(Popup):
+    pass
+
+
+class Sudoku(MDFloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.popup = SolvePopup()
+
+    def solve(self):
+
+        # Reset i/o files
+        # File output sebelumnya perlu dihapus biar ga baca yg sebelumnya
+        # Unkomen dibawah kalo kode ngubah output minisat ke gui udh jadi
+        # output_path = "io/gui_output.txt"
+        # if os.path.exists(output_path):
+        #     os.remove(output_path)
+        self.write_input()
+        self.read_output()
+
+    def write_input(self):
+        input_path = "io/gui_input.txt"
+        with open(input_path, "w") as f:
             boxes = self.ids.board.children[::-1]
-            for idx, box in enumerate(boxes):
-                if box.value is not None:
-                    print((idx // 4) + 1, (idx % 4) + 1, box.value, file=f)
+            for idx1, box in enumerate(boxes):
+                cells = box.ids.box.children[::-1]
+                for idx2, cell in enumerate(cells):
+                    if cell.value is not None:
+                        box_coords = (idx1 // 4, idx1 % 4)
+                        cell_coords = (idx2 // 4, idx2 % 4)
+                        print(
+                            box_coords[0] * 4 + cell_coords[0] + 1,
+                            box_coords[1] * 4 + cell_coords[1] + 1,
+                            cell.value,
+                            file=f,
+                        )
+
+    def read_output(self):
+        output_path = "io/gui_output.txt"
+
+        while not os.path.exists(output_path):
+            time.sleep(0.1)
+
+        with open(output_path, "r") as f:
+            line = f.readline()
+            flag = line.split()[0]
+            grid = [["-" for x in range(16)] for y in range(16)]
+            if flag == "1":
+                line = f.readline()
+                while line:
+                    row, col, val = line.split()
+                    grid[int(row) - 1][int(col) - 1] = val
+                    line = f.readline()
+                self.update(grid)
+            else:
+                self.popup.open()
+
+    def update(self, grid):
+        boxes = self.ids.board.children[::-1]
+        for idx1, box in enumerate(boxes):
+            cells = box.ids.box.children[::-1]
+            for idx2, cell in enumerate(cells):
+                box_coords = (idx1 // 4, idx1 % 4)
+                cell_coords = (idx2 // 4, idx2 % 4)
+                x = box_coords[0] * 4 + cell_coords[0]
+                y = box_coords[1] * 4 + cell_coords[1]
+                cell.set_value(grid[x][y])
 
 
 class SudokuSolverApp(MDApp):
